4452b315f1213a35e7173e8b7f856a00a657ece3
Rob Elliott <ree@purdue.edu>
2024-11-01 10:12:56 -0400

    Added point values

--- mongo_filters_mflix.mongodb.js
+++ mongo_filters_mflix.mongodb.js
@@ -2,7 +2,7 @@
 use('sample_mflix');
 
 
-/* IA QUESTION 1
+/* IA QUESTION 1 - 3 points
 
 How many movies directed by Mel Brooks also had Mel Brooks in the cast?
 
@@ -10,7 +10,7 @@ How many movies directed by Mel Brooks also had Mel Brooks in the cast?
 
 
 
-/* IA QUESTION 2
+/* IA QUESTION 2 - 3 points
 
 How many movies have a runtime between 90 and 120 minutes (inclusive) and are in the Comedy genre?
 
@@ -18,7 +18,7 @@ How many movies have a runtime between 90 and 120 minutes (inclusive) and are in
 
 
 
-/* IA QUESTION 3
+/* IA QUESTION 3 - 3 points
 
 How many movies have both "Adventure" and "Fantasy" genres and were released before the year 2010?
 
@@ -26,28 +26,28 @@ How many movies have both "Adventure" and "Fantasy" genres and were released bef
 
 
 
-/* IA QUESTION 4
+/* IA QUESTION 4 - 4 points
 
 How many movies are available in the Polish or German languages, have at least 1000 IMDB votes, and were released after 1996?
 
 */
 
 
-/* IA QUESTION 5
+/* IA QUESTION 5 - 4 points
 
 How many movies have a "Tomatoes" viewer rating greater than 80, a "Tomatoes" critic rating greater than 70, were released in the 1990s (including 1990), and are in the "Drama" genre?
 
 */
 
 
-/* IA QUESTION 6
+/* IA QUESTION 6 - 4 points
 
 How many movies have both "Comedy" and "Romance" genres, were released in the 2000s (including 2000), have a runtime less than 100 minutes, and have a "PG" rating?
 
 */
 
 
-/* IA QUESTION 7
+/* IA QUESTION 7 - 4 points
 
 How many movies have a metacritic score between 80 and 90 (inclusive), are in the "Thriller" or "Horror" genres, were released in the 2010s, and have a "R" rating?
 
